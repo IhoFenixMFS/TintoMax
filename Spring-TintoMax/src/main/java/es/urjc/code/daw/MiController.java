4c41e611677ebc3bef11e1a49df9e37a0b2e613b
Khenji55 <khenji55@gmail.com>
2018-02-22 00:06:31 +0100

    Add files via upload

---
+++ Spring-TintoMax/src/main/java/es/urjc/code/daw/MiController.java
@@ -0,0 +1,57 @@
+package es.urjc.code.daw;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MiController {
+
+	@RequestMapping("/home")
+	public String home(Model model) {
+
+	//	model.addAttribute("name", "World");
+
+		return "home";
+	}
+	@RequestMapping("/")
+	public String index(Model model) {
+
+	//	model.addAttribute("name", "World");
+
+		return "home";
+	}
+	@RequestMapping("/servicios_cliente")
+	public String servicios_cliente(Model model) {
+
+	//	model.addAttribute("name", "World");
+
+		return "servicios_cliente";
+	}
+	
+	@RequestMapping("/datos_cliente")
+	public String datos_cliente(Model model) {
+
+	//	model.addAttribute("name", "World");
+
+		return "datos_cliente";
+	}
+	@RequestMapping("/login")
+	public String login(Model model) {
+
+	//	model.addAttribute("name", "World");
+
+		return "login";
+	}
+	@RequestMapping("/precios")
+	public String precios(Model model) {
+
+	//	model.addAttribute("name", "World");
+
+		return "listado_precios";
+	}
+	
+}
