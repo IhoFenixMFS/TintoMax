fd7d6d527534468e147c78c75e80a34721fccda7
Marina Fernandez <m.fernandezsu@alumnos.urjc.es>
2018-02-24 17:29:30 +0100

    rect_datos

---
+++ sql-scripts/load_data.sql
@@ -0,0 +1,41 @@
+-- --------------------------------------------------------
+--
+-- Volcado de datos para la tabla `prendas`
+--
+
+INSERT INTO `prendas` (`num_prenda`, `nombre`, `limpieza_siva`, `plancha_siva`, ) VALUES(1, 'pantalon', 5, , 4.1, );
+INSERT INTO  (`num_prenda`, `nombre`, `limpieza_siva`,, `plancha_siva` ) VALUES(2, 'camisa', 3.02, 2.36);
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`t_user`, `dni`, `nombre`, `apellidos`, `direccion`, `telf`, `email`, `fecha_alta`, `password`) VALUES('admin', '00000000A', 'Administrador', 'Admin', 'TintoMax', 0, 'admin@admin.admin', '2018-02-01', 'admin');
+INSERT INTO `usuarios` (`t_user`, `dni`, `nombre`, `apellidos`, `direccion`, `telf`, `email`, `fecha_alta`, `password`) VALUES('empleado', '00000001M', 'Marina', 'Fdez. Sues', 'C/ Valladolid', 600000001, 'marina@empleada.email', '2018-02-12', '1-mar');
+INSERT INTO `usuarios` (`t_user`, `dni`, `nombre`, `apellidos`, `direccion`, `telf`, `email`, `fecha_alta`, `password`) VALUES('cliente', '00000002A', 'Andrea', 'Ch. Sues', 'C/ Sagunto', 600000002, 'andrea@clienta.email', '2018-02-12', '2-and');
+
+--
+-- Volcado de datos para la tabla `ticket`
+--
+
+
+INSERT INTO `ticket` (`num_ticket`, `id_prenda`, `prenda`, `cantidad`, `servicio`, `im_unit_siva`, `total_siva`, `Observaciones`) VALUES(1, 1, 'pantalon', 1, 'Limpieza', 5, 5, 'En proceso', NULL);
+INSERT INTO `ticket` (`num_ticket`, `id_prenda`, `prenda`, `cantidad`, `servicio`, `im_unit_siva`, `total_siva`, `Observaciones`) VALUES(1, 2, 'camisa', 2, 'Limpieza', 3.02, 6.04, 'En proceso', NULL);
+INSERT INTO `ticket` (`num_ticket`, `id_prenda`, `prenda`, `cantidad`, `servicio`, `im_unit_siva`, `total_siva`, `Observaciones`) VALUES(2, 1, 'pantalon', 5, 'Plancha', 4.1, 20.50, 'Listo', 'Sin ralla.');
+
+--
+-- Volcado de datos para la tabla `recibos`
+--
+
+INSERT INTO `recibos` (`num_ticket`, `tot_prendas`, `DNI_cliente`, `Atendido`, `fecha_entrada`, `salida_apx`, `salida_real`, `Estado`, `tot_siva`) VALUES(1, 3, '00000002A', '2018-02-18', '2018-02-28', '0000-00-00', 'En proceso', 11.04);
+INSERT INTO `recibos` (`num_ticket`, `tot_prendas`, `DNI_cliente`, `Atendido`, `fecha_entrada`, `salida_apx`, `salida_real`, `Estado`, `tot_siva`) VALUES(2, 5, '00000002A', '2018-02-24', '2018-02-24', '0000-00-00', 'Listo', 20.04);
+
+--
+-- Volcado de datos para la tabla `facturas`
+--
+
+INSERT INTO `facturas` (`num_factura`, `dni`, `nombre`, `fecha`, `num_ticket`, `importe_siva`) VALUES(1, '00000002A', 'Andrea', '2018-02-25', 2, 20.04);
+
+--
+-- Restricciones para tablas volcadas
+--
---
+++ sql-scripts/springapp.sql
@@ -0,0 +1,69 @@
+
+-- Base de datos: tintomax
+--
+CREATE DATABASE IF NOT EXISTS tintomax;
+
+USE tintomax;
+
+
+CREATE TABLE IF NOT EXISTS prendas (
+  num_prenda int(11) PRIMARY KEY AUTO_INCREMENT,
+  nombre varchar(25) NOT NULL UNIQUE,
+  limpieza_siva double NOT NULL,
+  plancha_siva double NOT NULL
+) ENGINE=InnoDB;
+
+
+CREATE TABLE IF NOT EXISTS usuarios (
+  id_user int(11) NOT NULL AUTO_INCREMENT,
+  t_user ENUM ('admin','empleado','cliente'),
+  dni varchar(11) NOT NULL,
+  nombre varchar(20) NOT NULL,
+  apellidos varchar(25) NOT NULL,
+  direccion varchar(50) NOT NULL,
+  telf int(11) NOT NULL,
+  email varchar(30) NOT NULL,
+  fecha_alta date NOT NULL,
+  password varchar(5) NOT NULL,
+  PRIMARY KEY (id_user),
+  UNIQUE KEY DNI_2 (DNI,Nombre)
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS ticket (
+  num_ticket int(11) NOT NULL,
+  id_prenda int(11) NOT NULL,
+  prenda varchar(25) NOT NULL UNIQUE,
+  cantidad int(11) NOT NULL,
+  servicio enum('Limpieza','Plancha') NOT NULL DEFAULT 'Limpieza',
+  im_unit_siva double NOT NULL,
+  total_siva double NOT NULL,
+  observaciones longtext,
+  PRIMARY KEY (num_ticket,id_prenda),
+  FOREIGN KEY (id_prenda) REFERENCES prendas(num_prenda),
+  FOREIGN KEY (prenda) REFERENCES prendas(nombre)
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS recibos (
+  num_ticket int(11) PRIMARY KEY ,
+  tot_prendas int(11) NOT NULL,
+  dni_cliente varchar(11) NOT NULL,
+  fecha_entrada date NOT NULL, 
+  salida_apx date NOT NULL,
+  salida_real date,
+  estado enum('En proceso','Listo','Recogido') DEFAULT 'En proceso',
+  tot_siva double NOT NULL,
+  FOREIGN KEY (num_ticket) REFERENCES ticket(num_ticket),
+  FOREIGN KEY (dni_cliente) REFERENCES usuarios(dni)
+) ENGINE=InnoDB;
+
+
+CREATE TABLE IF NOT EXISTS facturas (
+  num_factura int(11) PRIMARY KEY AUTO_INCREMENT,
+  dni varchar(11) NOT NULL,
+  nombre varchar(20) NOT NULL,
+  fecha date NOT NULL,
+  num_ticket int(11) NOT NULL,
+  importe_siva double NOT NULL,
+  FOREIGN KEY (num_ticket) REFERENCES recibos(num_ticket), 
+  FOREIGN KEY (dni) REFERENCES usuarios(dni)
+) ENGINE=InnoDB;
