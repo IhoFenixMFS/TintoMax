b29e5e35dec83216f38e05fa9c61762bd4f7d7ae
AlejandroGarciaLuna <alex.legent@gmail.com>
2018-02-28 19:18:51 +0100

    Update GarmentRepository.java

--- Spring-TintoMax/src/main/java/es/tintomax/server/repositories/GarmentRepository.java
+++ Spring-TintoMax/src/main/java/es/tintomax/server/repositories/GarmentRepository.java
@@ -1,9 +1,13 @@
 package es.tintomax.server.repositories;
 
 import es.tintomax.server.jpa.Garment;
-import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 public interface GarmentRepository extends CrudRepository<Garment, Integer> {
 
+	Page <Garment> findAll(Pageable page);
+	
 }
